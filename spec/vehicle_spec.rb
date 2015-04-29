8aaef21cfeff8c29ef401c076b1e7fc383d89c42
Jim Patt & ian bevel <student@epicodus.com>
2015-04-29 09:19:58 -0700

    add first spec return vehicle age

---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('vehicle')
+
+describe(Vehicle) do
+    describe("#age") do
+        it("returns the vehicles age") do
+            test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+            expect(test_vehicle.age()).to(eq(15))
+        end
+    end
+
+
+
+end
