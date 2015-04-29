e2bf5b7ee4a24e9d5888a46432eb0cc28fdb36af
Jim Patt & ian bevel <student@epicodus.com>
2015-04-29 12:11:22 -0700

    pass find spec

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -50,5 +50,14 @@ describe(Dealership) do
         end
     end
 
+    describe('.find') do
+        it('returns a dealership by its id number') do
+            test_dealership = Dealership.new("Bob's Used Cars")
+            test_dealership.save()
+            test_dealership2 = Dealership.new("Jane's Cars")
+            test_dealership2.save()
+            expect(Dealership.find(test_dealership.id())).to(eq(test_dealership))
+        end
+    end
 
 end
