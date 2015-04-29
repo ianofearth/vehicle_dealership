c9f2e0d15efce30dd87a8f33ce57463b1c5cc489
Jim Patt & ian bevel <student@epicodus.com>
2015-04-29 11:44:53 -0700

    add a dealership id spec

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -12,4 +12,13 @@ describe(Dealership) do
             expect(test_dealership.name()).to(eq("Bob's Used Cars"))
         end
     end
+
+    describe('#id') do
+        it("returns the id of the dealership") do
+            test_dealership = Dealership.new("Bob's Used Cars")
+            expect(test_dealership.id()).to(eq(1))
+        end
+    end
+
+    
 end
