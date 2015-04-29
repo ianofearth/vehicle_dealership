57d06ed8719df24dbd6dd9a332b3fe5dbf7301e7
Jim Patt & ian bevel <student@epicodus.com>
2015-04-29 09:35:54 -0700

    add and pass second spec to detirmine if vehicle is worth buying

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -9,4 +9,11 @@ class Vehicle
         current_year = Time.new().year()
         current_year - @year
     end
+
+    define_method(:worth_buying?) do
+        american_cars = ["Chrysler", "Ford", "GM"]
+        american = american_cars.include?(@make)
+        new_enough = self.age().<=(15)
+        american.&(new_enough)
+    end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -9,7 +9,12 @@ describe(Vehicle) do
         end
     end
 
-
+    describe('#worth_buying?') do
+        it ("returns false if the vehicle is not american made and younger than 16 years old") do
+            test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+            expect(test_vehicle.worth_buying?()).to(eq(false))
+        end
+    end
 
 
 end
