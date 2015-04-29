26c342325e790fdbf0062c41727d51f6d9d19382
Jim Patt & ian bevel <student@epicodus.com>
2015-04-29 11:42:10 -0700

    add code to pass dealership name spec

---
+++ lib/dealership.rb
@@ -0,0 +1,16 @@
+class Dealership
+    @@dealerships = []
+
+    define_method(:initialize) do |name|
+        @name = name
+        @id = @@dealerships.length().+(1)
+        @cars = []
+    end
+
+    define_method(:name) do
+        @name
+    end
+
+
+
+end
---
+++ spec/dealership_spec.rb
@@ -0,0 +1,15 @@
+require('dealership')
+require('rspec')
+
+describe(Dealership) do
+    # before() do
+    #     Dealership.clear()
+    # end
+
+    describe('#name') do
+        it('returns the name of the dealership') do
+            test_dealership = Dealership.new("Bob's Used Cars")
+            expect(test_dealership.name()).to(eq("Bob's Used Cars"))
+        end
+    end
+end
